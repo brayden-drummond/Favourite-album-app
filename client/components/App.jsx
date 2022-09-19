0d8ec80694bff55a8e6ca5e931d8d7f2647f51bf
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-20 11:45:42 +1200

    initial setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,27 +1,5 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 
-import { fetchFruits } from '../actions'
-
-function App() {
-  const fruits = useSelector((state) => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
-
-  return (
-    <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map((fruit) => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
-    </>
-  )
+export default function App() {
+  return <h2>Hello</h2>
 }
-
-export default App
