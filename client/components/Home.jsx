485786f415bc1437f2ad5e5f32ec171da3f6e1df
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-20 16:46:01 +1200

    reducer/ action and reducer test

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { fetchHomeContent } from '../actions/home'
+
 export default function Home() {
   return <h2>Home</h2>
 }
---
+++ client/reducers/__tests__/home.test.js
@@ -0,0 +1,24 @@
+import { SET_HOME_CONTENT } from '../../actions/home'
+import home from '../home'
+
+const mockHomeContentData = {
+  id: 1,
+  uploader_id: '1',
+  name: 'Top Gun Maverick',
+  description: 'Tom Cruise Placeholder',
+  image_url: '/images/topgun-maverick.jpg',
+}
+
+describe('home reducer', () => {
+  it('returns action payload for the type SET_HOME_CONTENT', () => {
+    const action = {
+      type: SET_HOME_CONTENT,
+      payload: mockHomeContentData,
+    }
+    const initialState = {}
+    const expectedState = mockHomeContentData
+    const outputState = home(initialState, action)
+    expect(outputState).toEqual(expectedState)
+    expect(outputState).not.toEqual(initialState)
+  })
+})
