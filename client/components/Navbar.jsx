a6027f94d34e044707a293e7aff198a5c92ccad5
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-20 14:58:17 +1200

    navbar component setup

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
-  return <h2>Navbar</h2>
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <div className="navbar-title">Movies</div>
+      </Link>
+      <div className="navbar-links">
+        <Link to="/play">Play</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/results">Results</Link>
+        <div className="navbar-login">Register | Log In | Log Out</div>
+      </div>
+    </nav>
+  )
 }
