5a00201f888c4a50c5422e82012d5beaab4eb714
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 14:02:18 +1300

    winner route

--- server/routes/winner.js
+++ server/routes/winner.js
@@ -1,5 +1,21 @@
 const express = require('express')
 
+const db = require('../db/winner')
 const router = express.Router()
+
+//add checkJwt
+router.post('/', (req, res) => {
+  const movies_id = req.body.food_id
+  const auth0_id = req.user?.sub
+  const created = new Date(Date.now())
+  db.addResult({ movies_id, auth0_id, created })
+    .then(() => {
+      res.sendStatus(204)
+    })
+    .catch((err) => {
+      console.error(err.message)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
 
 module.exports = router
