6b3e033688d50b27a24ef756a2eac4c7b25facd6
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 14:16:33 +1300

    winner reducer and actions

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import home from './home'
 import play from './play'
+import winner from './winner'
 
 export default combineReducers({
   home,
   play,
+  winner,
 })
