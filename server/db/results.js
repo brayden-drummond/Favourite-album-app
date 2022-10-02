7205a130812bdcd38486c9804515ec0977dbc365
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-03 08:42:27 +1300

    results database

---
+++ server/db/results.js
@@ -0,0 +1,15 @@
+const connection = require('./connection')
+
+function getResultsByAuth0Id(auth0Id, db = connection) {
+  return db('results')
+    .join('movies', 'movies_id', 'id')
+    .where('results.auth0_id', 'auth0id')
+    .select(
+      'movies.name as movieName',
+      'results.created as createdDate',
+      'movies.description as movieDescription',
+      'movies.image_url, as movieImage'
+    )
+}
+
+module.exports = { getResultsByAuth0Id }
