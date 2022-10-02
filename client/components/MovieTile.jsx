dccd9e22683c29fce07e46712a32fca67494a8ad
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 16:03:29 +1300

    play update

---
+++ client/components/MovieTile.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { fetchPlayContent } from '../actions/play'
+import styles from './MovieTile.module.scss'
+
+export default function FoodTile() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const movies = useSelector((state) => state.play)
+
+  const movieA = movies[0]
+  const movieB = movies[1]
+
+  useEffect(() => {
+    //pass in if (token)
+    dispatch(fetchPlayContent())
+  }, [])
+
+  return (
+    <div>
+      <p>{movieA}</p>
+    </div>
+  )
+}
