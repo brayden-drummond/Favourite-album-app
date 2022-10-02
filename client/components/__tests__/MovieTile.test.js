c84be103763bd9958b54134b384cff1bd035fadd
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-03 08:17:10 +1300

    movie tile test 2

--- client/components/__tests__/MovieTile.test.js
+++ client/components/__tests__/MovieTile.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import MovieTile from '../MovieTile'
@@ -26,9 +26,6 @@ const mockMovies = [
 const fakeDispatch = jest.fn()
 jest.mock('../../actions/play')
 
-const mockMoviesReturn = () => 'mockReturn'
-fetchPlayContent.mockReturnValue(mockMoviesReturn)
-
 const fakeStore = {
   subscribe: jest.fn(),
   dispatch: jest.fn(),
@@ -52,8 +49,11 @@ describe('<MovieTile />', () => {
     )
     const images = screen.getAllByRole('img', { exact: false })
     expect(images).toBeTruthy()
+    expect(images[0]).toHaveAttribute('src', mockMovies[0].imageUrl)
   })
   it('dispatches fetchPlayContent thunk on useEffect()', () => {
+    const mockMoviesReturn = () => 'mockReturn'
+    fetchPlayContent.mockReturnValue(mockMoviesReturn)
     render(
       <Provider store={fakeStore}>
         <BrowserRouter>
@@ -62,5 +62,35 @@ describe('<MovieTile />', () => {
       </Provider>
     )
     expect(fakeStore.dispatch).toHaveBeenCalledWith(mockMoviesReturn)
+  })
+  it('deletes opposing image onClick when using button #1', () => {
+    const deletePlayContentMockReturn = jest.fn()
+    deletePlayContent.mockReturnValue(deletePlayContentMockReturn)
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <MovieTile />
+        </BrowserRouter>
+      </Provider>
+    )
+    const button = screen.getAllByRole('button')
+    fireEvent.click(button[0])
+    // expect(fakeDispatch).toHaveBeenCalledWith(deletePlayContentMockReturn)
+    expect(deletePlayContent.mock.calls[0][0]).toBe(mockMovies[1].id)
+  })
+  it('deletes opposing image onClick when using button #2', () => {
+    const deletePlayContentMockReturn = jest.fn()
+    deletePlayContent.mockReturnValue(deletePlayContentMockReturn)
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <MovieTile />
+        </BrowserRouter>
+      </Provider>
+    )
+    const button = screen.getAllByRole('button')
+    fireEvent.click(button[1])
+    // expect(fakeDispatch).toHaveBeenCalledWith(deletePlayContentMockReturn)
+    expect(deletePlayContent.mock.calls[0][0]).toBe(mockMovies[0].id)
   })
 })
