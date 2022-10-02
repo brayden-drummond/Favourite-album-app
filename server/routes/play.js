cf8a30ea8da30b48a00de644c9b7bc7d23a5ff62
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 15:09:15 +1300

    play route

--- server/routes/play.js
+++ server/routes/play.js
@@ -1,5 +1,18 @@
 const express = require('express')
-
+const db = require('../db/play')
 const router = express.Router()
+// const checkJwt = require('../auth0')
+
+//pass in checkJwt
+router.get('/', (req, res) => {
+  db.getMovies()
+    .then((results) => {
+      res.json(results)
+    })
+    .catch((err) => {
+      console.error(err.message)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
 
 module.exports = router
