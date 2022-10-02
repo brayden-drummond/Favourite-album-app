0aa180cf720b65712324b8d41487bb4217b0810d
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 15:35:15 +1300

    weird error with play

--- server/db/play.js
+++ server/db/play.js
@@ -1,7 +1,13 @@
 const connection = require('./connection')
 
 function getMovies(db = connection) {
-  return db('movies').select()
+  return db('movies').select(
+    'id',
+    'uploader_id as uploaderId',
+    'name',
+    'description',
+    'image_url as imageUrl'
+  )
 }
 
 module.exports = { getMovies }
