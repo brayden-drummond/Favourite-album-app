7fe34435ad4bf438fb76fb57a721cea89f15b285
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 13:33:42 +1300

    home data rendering

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import styles from './Home.module.scss'
 
 import { fetchHomeContent } from '../actions/home'
 
 export default function Home() {
-  return <h2>Home</h2>
+  const dispatch = useDispatch()
+  const homeContent = useSelector((state) => state.home)
+
+  useEffect(() => dispatch(fetchHomeContent()), [])
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to your favourite Movie App!</h1>
+      <div className={styles.text}>
+        <p>Will your favourite movie be {homeContent.name}?</p>
+        <img
+          className={styles.image}
+          src={homeContent.imageUrl}
+          alt={homeContent.description}
+        ></img>
+        <p>Click Play to find out!</p>
+      </div>
+      <div className={styles.buttoncontainer}>
+        <Link to='/play'>
+          <button className={styles.button}>Play!</button>
+        </Link>
+      </div>
+    </div>
+  )
 }
