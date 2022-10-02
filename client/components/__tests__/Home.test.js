f6e91ed77265fd44aaa758ae966918c936c06fb6
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 13:44:01 +1300

    passing home component test

---
+++ client/components/__tests__/Home.test.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux'
+import { screen, render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import Home from '../Home'
+import { fetchHomeContent } from '../../actions/home'
+
+const mockHomeData = {
+  name: 'mockName',
+  imageUrl: 'mockImageUrl',
+  description: 'mockDescription',
+}
+
+jest.mock('../../actions/home')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+const fakeStore = {
+  subscribe: jest.fn(),
+  dispatch: jest.fn(),
+  getState: jest.fn(() => {
+    return { home: mockHomeData }
+  }),
+}
+
+describe('<Home />', () => {
+  it('displays image, foodname and description from redux state.', () => {
+    render(
+      <Provider store={fakeStore}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </Provider>
+    )
+    const movieName = screen.getByText(mockHomeData.name, { exact: false })
+    const movieImage = screen.getByRole('img')
+    expect(movieName).toBeTruthy()
+    expect(movieImage.src).toMatch(mockHomeData.imageUrl)
+  })
+})
