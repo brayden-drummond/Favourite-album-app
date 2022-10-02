8ce8858e9f771754f7b7101b2b81f04e9bd157fe
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-02 14:11:22 +1300

    tests for winner route

---
+++ server/routes/__tests__/winner.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest')
+const server = require('../../server')
+
+const { addResult } = require('../../db/winner')
+
+jest.mock('../../db/winner')
+jest.spyOn(console, 'error')
+
+afterEach(() => {
+  console.error.mockReset()
+})
+
+// const checkJwt = require('../../auth0')
+// jest.mock('../../auth0')
+
+// checkJwt.mockImplementation((req, res, next) => {
+//   req.user = { sub: 'testAuth0Id' }
+//   next()
+// })
+
+describe('POST /api/v1/winner', () => {
+  it('post result to results array', () => {
+    addResult.mockReturnValue(Promise.resolve([7]))
+    return request(server)
+      .post('/api/v1/winner')
+      .send({ movies_id: 4 })
+      .then((res) => {
+        console.log(addResult.mock.calls[0][0])
+        expect(res.status).toBe(204)
+        expect(addResult.mock.calls[0][0].movies_id).toBe(4)
+        return null
+      })
+  })
+  it('return status 500 and consoles error when problem', () => {
+    addResult.mockImplementation(() => Promise.reject(new Error('fail')))
+    console.error.mockImplementation(() => {})
+    return request(server)
+      .post('/api/v1/winner')
+      .then((res) => {
+        expect(res.status).toBe(500)
+        expect(console.error).toHaveBeenCalledWith('fail')
+        return null
+      })
+  })
+})
--- server/routes/winner.js
+++ server/routes/winner.js
@@ -4,9 +4,11 @@ const db = require('../db/winner')
 const router = express.Router()
 
 //add checkJwt
+// const auth0_id = req.user?.sub
+
 router.post('/', (req, res) => {
-  const movies_id = req.body.food_id
-  const auth0_id = req.user?.sub
+  const movies_id = req.body.movies_id
+  const auth0_id = 'brayden'
   const created = new Date(Date.now())
   db.addResult({ movies_id, auth0_id, created })
     .then(() => {
