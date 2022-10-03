a73a02ece5ff0b2def5ef311947d819f2654735d
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-03 17:14:07 +1300

    results component

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import styles from './Results.module.scss'
 
+import { fetchResultsContent } from '../actions/results'
 export default function Results() {
-  return <h2>Results</h2>
+  const dispatch = useDispatch()
+  const results = useSelector((state) => state.results)
+  // const token = useSelector((state) => state.token)
+
+  useEffect(() => {
+    //pass in token
+    dispatch(fetchResultsContent())
+  }, [])
+  return (
+    <>
+      <h1 className={styles.heading}>Here are your results!</h1>
+      <div className={styles.container}>
+        {results.map((result, i) => {
+          return (
+            <div key={i} className={styles.movieContainer}>
+              <img
+                className={styles.image}
+                src={results.movieImage}
+                alt={results.movieName}
+              />
+              <p>{results.movieName}</p>
+              <p>{results.description}</p>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
