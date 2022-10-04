92ca61b7339614a6f54a963656a13c73e6ccb8f6
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-05 08:44:39 +1300

    lib.js for create

--- server/routes/create.js
+++ server/routes/create.js
@@ -1,5 +1,9 @@
 const express = require('express')
 
+const { addMovie } = require('../db/create')
+const { getMovies } = require('../db/home')
+//require in dotenv
+//require in ./lib
 const router = express.Router()
 
 module.exports = router
