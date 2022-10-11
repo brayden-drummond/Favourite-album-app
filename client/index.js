476e4db9c1f3a6401ef23b5338ece25f0be616dc
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-12 12:07:49 +1300

    start of auth0 integration

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import store from './store'
@@ -8,11 +9,18 @@ import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
+    <Auth0Provider
+      domain='pikopiko-2022-brayden.au.auth0.com'
+      clientId='7raSlEazscGTBtY6AggUsLVeLKk880AS'
+      redirectUri={window.location.origin}
+      audience='https://movies/api'
+    >
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </Auth0Provider>,
     document.getElementById('app')
   )
 })
