0869079f745ec9f0d103e3eeb6675bac27f344df
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-13 12:53:21 +1300

    update for Navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 import { Link } from 'react-router-dom'
 
 import styles from './Navbar.module.scss'
 
 export default function Navbar() {
+  const { isAuthenticated, logout, loginWithRedirect } = useAuth0()
+
+  const handleLogOff = (e) => {
+    e.preventDefault()
+    logout()
+  }
+
+  const handleSignIn = (e) => {
+    e.preventDefault()
+    loginWithRedirect()
+  }
+
   return (
     <nav className={styles.navbar}>
       <Link to='/'>
@@ -14,7 +27,19 @@ export default function Navbar() {
         <Link to='/play'>Play</Link>
         <Link to='/create'>Add Movie</Link>
         <Link to='/results'>Results</Link>
-        <div className='navbar-login'>Register | Log In | Log Out</div>
+        {isAuthenticated ? (
+          <div className={styles.auth}>
+            <Link to='/' onClick={handleLogOff}>
+              Log Out
+            </Link>
+          </div>
+        ) : (
+          <div className={styles.auth}>
+            <Link to='/' onClick={handleSignIn}>
+              Log In
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   )
