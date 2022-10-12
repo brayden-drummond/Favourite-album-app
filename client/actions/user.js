d75538393331e795a3ee31953d07012836ffa9c0
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-10-13 09:01:21 +1300

    action for user including get to display user

---
+++ client/actions/user.js
@@ -0,0 +1,25 @@
+import { getUser } from '../apis/user'
+
+export const UPDATE_LOGGED_IN_USER = 'UPDATE_LOGGED_IN_USER'
+export const CLEAR_LOGGED_IN_USER = 'CLEAR_LOGGED_IN_USER'
+
+export function updateLoggedInUser(userToSave) {
+  return {
+    type: UPDATE_LOGGED_IN_USER,
+    payload: userToSave,
+  }
+}
+
+export function clearLoggedInUser() {
+  return {
+    type: UPDATE_LOGGED_IN_USER,
+  }
+}
+
+export function fetchUser(token) {
+  return (dispatch) => {
+    return getUser(token)
+      .then((user) => dispatch(updateLoggedInUser(user)))
+      .catch((err) => console.error(err.message))
+  }
+}
